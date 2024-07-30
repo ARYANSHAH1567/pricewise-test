6526a390031d25e78e0c1f35a513455447ebdf73
Aryan Shah <aryanashah11@gmail.com>
2024-07-30 19:06:32 +0530

    second commit

--- lib/models/users.models.ts
+++ lib/models/users.models.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    clerkId: {type:Number,required:true},
+    clerkId: {type:String,required:true},
     email: { type: String, required: true, unique: true},
     TrackedProd: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', default: []}],
     PastProd: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', default: []}],
